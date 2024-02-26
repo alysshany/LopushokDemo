8eab9b659cbd1f0a13130a222084c65c886aa7d0
Zilya <galievaaziz@gmail.com>
2024-02-26 17:33:22 +0300

    добавила partial class

---
+++ Lopushok/LopushokWpf/ADO/PartialClasses/Product.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace LopushokWpf.ADO
+{
+	public partial class Product
+	{
+		public decimal Sum => ProductMaterial.Sum(x => x.MaterialCount * x.Material.Cost);
+		public BitmapImage ProductImage
+		{
+			get
+			{
+				var bitmap = new BitmapImage();
+				byte[] bytes;
+
+				if (Image == null || Image == "" || Image == "нет" || Image == "не указано")
+				{
+					bytes = File.ReadAllBytes(Directory.GetParent(Environment.CurrentDirectory).Parent.FullName +
+											  $"\\Images\\picture.png");
+				}
+				else
+				{
+					bytes = File.ReadAllBytes(Directory.GetParent(Environment.CurrentDirectory).Parent.FullName +
+											  $"\\Images{Image}");
+				}
+
+				var ms = new MemoryStream(bytes);
+
+
+				bitmap.BeginInit();
+				bitmap.StreamSource = ms;
+				bitmap.EndInit();
+				return bitmap;
+			}
+		}
+	}
+}
