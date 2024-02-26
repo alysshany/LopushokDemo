a783b41655a789cd58e4d4738e7d30e559bb3da8
Zilya <galievaaziz@gmail.com>
2024-02-26 18:24:10 +0300

    что-то почти конечное

--- Lopushok/LopushokWpf/Pages/ItemsList.xaml.cs
+++ Lopushok/LopushokWpf/Pages/ItemsList.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LopushokWpf.ADO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,10 +21,141 @@ namespace LopushokWpf.Pages
 	/// </summary>
 	public partial class ItemsList : Page
 	{
+		private const int PageSize = 20;
+		private int CurrentPage = 0;
+		private int TotalCount;
+		private int PagesCount;
+		private string FilterName;
+		private string SearchFilter = "";
+		private string SortField;
+		private bool IsAsc = false;
+
+
 		public ItemsList()
 		{
 			InitializeComponent();
-			lvProducts.ItemsSource = App.Connection.Product.ToList();
+			var filters = new List<ProductType> { new ProductType { Name = "Все типы" } };
+			filters.AddRange(App.Connection.ProductType.ToList());
+			cbFilter.ItemsSource = filters;
+			cbFilter.SelectedIndex = 0;
+
+			var sorts = new List<string> {
+					"Без сортировки",
+					"Наименование", "Номер производственного цеха",
+					"Минимальная стоимость для агента"};
+			cbSort.ItemsSource = sorts;
+			cbSort.SelectedIndex = 0;
+
+			cbSortOrders.ItemsSource = new List<string> { "По убыванию", "По возрастанию" };
+			cbSortOrders.SelectedIndex = 0;
+			UpdateSource();
+		}
+
+		private void UpdateSource()
+		{
+			List<Product> products;
+			if (string.IsNullOrEmpty(FilterName) || FilterName == "Все типы")
+			{
+				products = App.Connection.Product.ToList();
+			}
+			else
+			{
+				products = App.Connection.Product.ToList().Where(x => x.ProductType.Name == FilterName).ToList();
+			}
+
+			products = products.Where(x => x.Name.ToLower().Contains(SearchFilter.ToLower())).ToList();
+
+			TotalCount = products.Count;
+			PagesCount = TotalCount / PageSize + (TotalCount % PageSize != 0 ? 1 : 0);
+			PagesListBox.ItemsSource = Enumerable.Range(0, PagesCount).Select(x => x + 1);
+
+			if (IsAsc)
+			{
+				if (SortField == "Наименование")
+				{
+					products = products.OrderBy(x => x.Name).ToList();
+				}
+				else if (SortField == "Номер производственного цеха")
+				{
+					products = products.OrderBy(x => x.NumberOfFactory).ToList();
+				}
+				else
+				{
+					products = products.OrderBy(x => x.MinimalAgentCost).ToList();
+				}
+			}
+			else
+			{
+				if (SortField == "Наименование")
+				{
+					products = products.OrderByDescending(x => x.Name).ToList();
+				}
+				else if (SortField == "Номер производственного цеха")
+				{
+					products = products.OrderByDescending(x => x.NumberOfFactory).ToList();
+				}
+				else
+				{
+					products = products.OrderByDescending(x => x.MinimalAgentCost).ToList();
+				}
+			}
+			lvProducts.ItemsSource = products.Skip(PageSize * CurrentPage).Take(PageSize);
+		}
+
+		private void PrevPageButton_Click(object sender, RoutedEventArgs e)
+		{
+			CurrentPage = Math.Max(0, CurrentPage - 1);
+			UpdateSource();
+		}
+
+		private void NextPageButton_Click(object sender, RoutedEventArgs e)
+		{
+			CurrentPage = Math.Min(PagesCount - 1, CurrentPage + 1);
+			UpdateSource();
+		}
+
+		private void PagesListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			var listBox = ((ListBox)sender).SelectedItem;
+			var pageNumber = (int)listBox - 1;
+			CurrentPage = pageNumber;
+			UpdateSource();
+		}
+
+		private void cbSortOrders_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			var order = (string)((ComboBox)sender).SelectedItem;
+			if (order == "По возрастанию")
+			{
+				IsAsc = true;
+			}
+			else
+			{
+				IsAsc = false;
+			}
+			UpdateSource();
+		}
+
+		private void cbSort_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			SortField = (string)((ComboBox)sender).SelectedItem;
+			UpdateSource();
+		}
+
+		private void cbFilter_SelectionChanged(object sender, SelectionChangedEventArgs e)
+		{
+			var selectedItem = (ProductType)((ComboBox)sender).SelectedItem;
+			if (selectedItem == null)
+				return;
+			FilterName = selectedItem.Name;
+			UpdateSource();
+		}
+
+		private void tbSearch_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			var text = ((TextBox)sender).Text;
+			SearchFilter = text;
+			UpdateSource();
 		}
 	}
 }
